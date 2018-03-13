b1ed47b1d95700e1e4d584a003f7fa851860b159
Christophe Hamerling <chamerling@linagora.com>
2018-03-14 00:00:39 +0100

    fixup

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
-import Vue from 'vue'
-import Vuetify from 'vuetify'
-import VueAxios from 'vue-axios'
+import Vue from 'vue';
+import Vuetify from 'vuetify';
+import VueAxios from 'vue-axios';
 import axios from 'axios';
 
 import App from './App';
